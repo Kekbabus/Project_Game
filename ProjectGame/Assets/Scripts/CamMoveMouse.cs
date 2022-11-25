f50981ad25cb37892ff111fe1a95967154f2a5b1
Rahavy <114474771+Rahavy@users.noreply.github.com>
2022-11-25 19:26:14 +0000

    updated movement

--- ProjectGame/Assets/Scripts/CamMoveMouse.cs
+++ ProjectGame/Assets/Scripts/CamMoveMouse.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class CamMoveMouse : MonoBehaviour
@@ -14,6 +15,7 @@ public class CamMoveMouse : MonoBehaviour
         //gets access to mouse up and down movements
         rotationY += Input.GetAxis("Mouse X") * sensitivity;
         rotationX += Input.GetAxis("Mouse Y") * -1 * sensitivity;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
         transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
     }
 }
